5611929bf6421141e375f1ebae73532ceca6daa2
alekscrosss <137340135+alekscrosss@users.noreply.github.com>
2024-02-08 17:43:38 +0100

    new

---
+++ myquotes/quotes/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Author(models.Model):
+    fullname = models.CharField(max_length=255)
+    born_date = models.CharField(max_length=255)
+    born_location = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.fullname
+
+class Quote(models.Model):
+    author = models.ForeignKey(Author, related_name='quotes', on_delete=models.CASCADE)
+    quote = models.TextField()
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.quote[:50]
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    fullname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255, blank=True, null=True)
+    message_sent = models.BooleanField(default=False)
+    preferred_contact_method = models.CharField(max_length=255, choices=[('email', 'Email'), ('sms', 'SMS')])
